6179e7840348f28112e4182e7814cf6cb0c3076c
ivaperma <ivanpm89@gmail.com>
2023-01-16 21:19:54 +0100

    added mapasApp

---
+++ 09-mapasApp/src/app/mapas/pages/full-screen/full-screen.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
+
+
+@Component({
+  selector: 'app-full-screen',
+  templateUrl: './full-screen.component.html',
+  styles: [
+    `
+    #mapa {
+      width: 100%;
+      height: 100%;
+    }
+    `
+  ]
+})
+export class FullScreenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+    var map = new mapboxgl.Map({
+      container: 'mapa',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [ -3.7328807833397426 , 40.33908789843816 ],
+      zoom: 15
+    });
+
+  }
+
+}
---
+++ 09-mapasApp/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  mapboxToken: 'pk.eyJ1IjoiaXZhbm44OSIsImEiOiJjbGN1ZW81ZnQwdHFrM3BteHB2cHZ4YWVyIn0.ODZJ-m21mAtEGIpUEpkcgQ'
+};
